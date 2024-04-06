326e5656cb2ca781b609c8557abb7b6663e2d181
AlexanderGorskikh <alexandergorsc@gmail.com>
2024-04-06 20:20:43 +0300

    Создание пустого проекта

---
+++ src/main/java/gb/SpringNotificationBot/SpringNotificationBotApplication.java
@@ -0,0 +1,13 @@
+package gb.SpringNotificationBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNotificationBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNotificationBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gb/SpringNotificationBot/SpringNotificationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.SpringNotificationBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNotificationBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
