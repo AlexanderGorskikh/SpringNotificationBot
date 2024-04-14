77ecf3027aa49d3d328735c8e17ea01dc033baf6
AlexanderGorskikh <alexandergorsc@gmail.com>
2024-04-14 12:14:14 +0300

    feat(keyboard): add inline keyboard

---
+++ src/main/java/cc/gb/SpringNotificationBot/model/CallbackDataType.java
@@ -0,0 +1,6 @@
+package cc.gb.SpringNotificationBot.model;
+
+public class CallbackDataType {
+    public static final String YES_BUTTON = "YES_BUTTON";
+    public static final String NO_BUTTON = "NO_BUTTON";
+}
--- src/main/java/cc/gb/SpringNotificationBot/services/TelegramBot.java
+++ src/main/java/cc/gb/SpringNotificationBot/services/TelegramBot.java
@@ -1,6 +1,7 @@
 package cc.gb.SpringNotificationBot.services;
 
 import cc.gb.SpringNotificationBot.config.BotConfiguration;
+import cc.gb.SpringNotificationBot.model.CallbackDataType;
 import cc.gb.SpringNotificationBot.model.User;
 import cc.gb.SpringNotificationBot.repository.UserRepository;
 import com.vdurmont.emoji.EmojiParser;
@@ -11,11 +12,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -29,7 +33,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Autowired
     private UserRepository userRepository;
     private final static String HELP_MESSAGE = "Welcome to our demonstration bot";
-    private ReplyKeyboardMarkup regularKeyboard;
+    private final ReplyKeyboardMarkup regularKeyboard;
 
     public TelegramBot(BotConfiguration botConfiguration) {
         this.botConfiguration = botConfiguration;
@@ -48,6 +52,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                 }
                 case "my_data" -> {
+
                 }
                 case "/delete_data" -> {
                 }
@@ -56,10 +61,74 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/help" -> {
                     sendMessage(chatId, HELP_MESSAGE, regularKeyboard);
                 }
+                case "/register" ->{
+                    register(chatId);
+                }
                 default -> {
                     sendMessage(chatId, "Пока функция не поддерживается, ", regularKeyboard);
                 }
             }
+        } else if (update.hasCallbackQuery()){
+            String callbackData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+            switch (callbackData){
+                case CallbackDataType.YES_BUTTON -> {
+                    String text = "You pressed YES button";
+                    EditMessageText editedMessage = new EditMessageText();
+                    editedMessage.setChatId(chatId);
+                    editedMessage.setText(text);
+                    editedMessage.setMessageId((int) messageId);
+                    try {
+                        execute(editedMessage);
+                    } catch (TelegramApiException e) {
+                        System.out.println("ошибка");
+                        log.error("Error occurred: " + e.getMessage());
+                    }
+                }
+                case CallbackDataType.NO_BUTTON -> {
+                    String text = "You pressed NO button";
+                    EditMessageText editedMessage = new EditMessageText();
+                    editedMessage.setChatId(chatId);
+                    editedMessage.setText(text);
+                    editedMessage.setMessageId((int) messageId);
+                    try {
+                        execute(editedMessage);
+                    } catch (TelegramApiException e) {
+                        System.out.println("ошибка");
+                        log.error("Error occurred: " + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    private void register(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Do you want to registered?");
+        var inlineKeyboard = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        var yesButton = new InlineKeyboardButton();
+
+        yesButton.setText("yes");
+        yesButton.setCallbackData(CallbackDataType.YES_BUTTON);
+
+        var noButton = new InlineKeyboardButton();
+        noButton.setText("no");
+        noButton.setCallbackData(CallbackDataType.NO_BUTTON);
+
+        rowInline.add(yesButton);
+        rowInline.add(noButton);
+        rowsInLine.add(rowInline);
+        inlineKeyboard.setKeyboard(rowsInLine);
+        message.setReplyMarkup(inlineKeyboard);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            System.out.println("ошибка");
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 
@@ -126,6 +195,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private void addListOfCommands() {
         List<BotCommand> listOfCommands = new ArrayList<>();
         listOfCommands.add(new BotCommand("/start", "start program"));
+        listOfCommands.add(new BotCommand("/register", "start register"));
         listOfCommands.add(new BotCommand("/my_data", "get data stored"));
         listOfCommands.add(new BotCommand("/delete_data", "delete my data"));
         listOfCommands.add(new BotCommand("/settings", "list of preferences"));
