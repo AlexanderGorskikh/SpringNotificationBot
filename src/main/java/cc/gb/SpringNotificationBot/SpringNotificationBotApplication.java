97161c9e1f5e4f259edce479d7a75762050481d5
lagos <lagoss@yandex.ru>
2024-04-21 19:37:07 +0300

    fix: reworking notification service, add multithreading support

--- src/main/java/cc/gb/SpringNotificationBot/SpringNotificationBotApplication.java
+++ src/main/java/cc/gb/SpringNotificationBot/SpringNotificationBotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SpringNotificationBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNotificationBotApplication.class, args);
--- src/main/java/cc/gb/SpringNotificationBot/services/TelegramNotificationService.java
+++ src/main/java/cc/gb/SpringNotificationBot/services/TelegramNotificationService.java
@@ -2,18 +2,21 @@ package cc.gb.SpringNotificationBot.services;
 
 import cc.gb.SpringNotificationBot.model.Event;
 import cc.gb.SpringNotificationBot.model.EventStatus;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 
 /**
  * Сервис, отвечающий за рассылку уведомлений,
  * использующий аннотацию Scheduled,
- * оповещающий пользователя о внесенным им уведомлени
+ * оповещающий пользователя о внесенным им уведомлении
  */
 @Component
+@EnableScheduling
 public class TelegramNotificationService {
 
     private final TelegramBotCRUDHandler crudHandler;
@@ -23,18 +26,32 @@ public class TelegramNotificationService {
     public TelegramNotificationService(TelegramBotCRUDHandler crudHandler, TelegramBot telegramBot) {
         this.crudHandler = crudHandler;
         this.telegramBot = telegramBot;
-        checkNotification();
     }
 
     @Scheduled(fixedDelay = 60000)
     public void checkNotification() {
         List<Event> notifications = crudHandler.getListEventsByStatus(EventStatus.PLANNED);
+        StringBuilder notification = new StringBuilder();
         for (Event event : notifications) {
+            LocalDateTime eventTime = event.getTimeOfNotification();
             LocalDateTime now = LocalDateTime.now();
-            if (event.getTimeOfNotification().isBefore(now)) {
-                String message = "У вас запланировано: " + event.getDescription() + " на " + event.getTimeOfNotification();
-                telegramBot.sendMessage(event.getUser().getChatId(), message, null);
-                System.out.println("Notification");
+            if (eventTime.isAfter(now) && eventTime.minusMinutes(30).isBefore(now)) {
+                notification.append("У вас запланировано: ");
+                notification.append(event.getDescription());
+                notification.append(" через полчаса!");
+                telegramBot.sendMessage(event.getUser().getChatId(), notification.toString(), null);
+                notification.setLength(0);
+            } else if (eventTime.isAfter(now) && eventTime.minusMinutes(10).isBefore(now)) {
+                notification.append("У вас запланировано: ");
+                notification.append(event.getDescription());
+                notification.append(" через десять минут!");
+                telegramBot.sendMessage(event.getUser().getChatId(), notification.toString(), null);
+                notification.setLength(0);
+            }
+            if (event.getTimeOfNotification().isAfter(now)) {
+                notification.append("У вас запланировано!");
+                notification.append(event.getDescription());
+                telegramBot.sendMessage(event.getUser().getChatId(), notification.toString(), null);
             }
         }
     }
